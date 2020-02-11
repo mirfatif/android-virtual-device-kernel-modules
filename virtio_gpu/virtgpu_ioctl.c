ac34f9990cb621c9969dc3125b376812b284b115
Gerd Hoffmann <kraxel@redhat.com>
2020-02-11 14:50:46 +0100

    drm/virtio: fix virtio_gpu_execbuffer_ioctl locking
    Lockdep says we can't call vmemdup() while having objects reserved
    because it needs the mmap semaphore.  So reorder the calls reserve
    the objects later.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20200211135047.22261-2-kraxel@redhat.com
    Change-Id: I90ad1f05b1611af41701f5be2ff37bd89f4d48a0

--- virtio_gpu/virtgpu_ioctl.c
+++ virtio_gpu/virtgpu_ioctl.c
@@ -178,22 +178,22 @@ static int virtio_gpu_execbuffer_ioctl(struct drm_device *dev, void *data,
 		bo_handles = NULL;
 	}
 
-	if (buflist) {
-		ret = virtio_gpu_array_lock_resv(buflist);
-		if (ret)
-			goto out_unused_fd;
-	}
-
 	buf = vmemdup_user(u64_to_user_ptr(exbuf->command), exbuf->size);
 	if (IS_ERR(buf)) {
 		ret = PTR_ERR(buf);
-		goto out_unresv;
+		goto out_unused_fd;
+	}
+
+	if (buflist) {
+		ret = virtio_gpu_array_lock_resv(buflist);
+		if (ret)
+			goto out_memdup;
 	}
 
 	out_fence = virtio_gpu_fence_alloc(vgdev);
 	if(!out_fence) {
 		ret = -ENOMEM;
-		goto out_memdup;
+		goto out_unresv;
 	}
 
 	if (out_fence_fd >= 0) {
@@ -213,11 +213,11 @@ static int virtio_gpu_execbuffer_ioctl(struct drm_device *dev, void *data,
 	dma_fence_put(&out_fence->f);
 	return 0;
 
-out_memdup:
-	kvfree(buf);
 out_unresv:
 	if (buflist)
 		virtio_gpu_array_unlock_resv(buflist);
+out_memdup:
+	kvfree(buf);
 out_unused_fd:
 	kvfree(bo_handles);
 	if (buflist)
