benji-cannot-code/fast-vc-linux-benchmark-run-3

drm/virtio: static-ify virtio_fence_signaled
Not used anywhere else.

Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
Link: http://patchwork.freedesktop.org/patch/msgid/20191219005733.18960-1-gurchetansingh@chromium.org
Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

drivers/gpu/drm/virtio/virtgpu_fence.c
@@ -38,7 +38,7 @@ static const char *virtio_get_timeline_name(struct dma_fence *f)
 	return "controlq";
 }
 
-bool virtio_fence_signaled(struct dma_fence *f)
+static bool virtio_fence_signaled(struct dma_fence *f)
 {
 	struct virtio_gpu_fence *fence = to_virtio_fence(f);
 

mirfatif/android-virtual-device-kernel-modules

Change-Id: I8c427df1fa468bef2bd94dedcef5143e33adc61d

virtio_gpu/virtgpu_fence.c
@@ -37,7 +37,7 @@ static const char *virtio_gpu_get_timeline_name(struct dma_fence *f)
 	return "controlq";
 }
 
-bool virtio_gpu_fence_signaled(struct dma_fence *f)
+static bool virtio_gpu_fence_signaled(struct dma_fence *f)
 {
 	/* leaked fence outside driver before completing
 	 * initialization with virtio_gpu_fence_emit.