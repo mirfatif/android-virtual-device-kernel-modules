287821d3d599fdffc103a845806fb3e3344b99fd
Tiffany Yang <ynaffit@google.com>
2024-02-06 18:50:35 -0800

    WIP: FUSE: set up module to implement tmpfiles and other vendor hooks
    The FUSE tmpfile implementation in Linux upstream cannot be backported
    to android13-5.15 because it breaks the ABI. Set up FUSE vendor module
    to provide provide a non-atomic implementation of tmpfiles that can
    connect to the vendor hooks in the common kernel. Allow handlers for
    other vendor hooks to be added easily.
    
    (this doesn't work because we are calling a private function)
    
    BUG=323975715
    BUG=318581825
    TEST=arc.Boot.vm
    
    Change-Id: I069f7943670d2c4891106e78cb5b1fac8cb4e1d8
    Signed-off-by: Tiffany Yang <ynaffit@google.com>

---
+++ include/uapi/linux/fuse_vendor.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_FUSE_VENDOR_H
+#define _LINUX_FUSE_VENDOR_H
+
+#include <linux/types.h>
+
+enum fuse_vendor_opcodes {
+	/*
+	 * Opcodes 1-52 and 2016 are reserved as of FUSE 7.39
+         */
+	FUSE_NONATOMIC_TMPFILE    = 4294967295,           /* UINT_MAX */
+};
+
+struct fuse_tmpfile_in {
+	uint32_t mode;
+	uint32_t umask;
+};
+
+#endif /* _LINUX_FUSE_VENDOR_H */
