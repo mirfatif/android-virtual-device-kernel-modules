63d46b62de21e08efe1ef054157bd14119c4caf0
Shalini S <shalinisen@google.com>
2024-05-10 18:28:48 +0000

    SignalSynchronizationObjectFromSyncFile api
    - Add context in dxgsyncpoint for handles/fences
    - Add api SignalSynchronizationObjectFromSyncFile. Registers callback on
      dma fence.
    - Implement callback
    
    Change-Id: I952ca5c105c23289cb48b64a90235e009c6d428e

--- virtio_dxgkrnl/dxgsyncfile.h
+++ virtio_dxgkrnl/dxgsyncfile.h
@@ -15,14 +15,42 @@
 #define _DXGSYNCFILE_H
 
 #include <linux/sync_file.h>
+#include <linux/mutex.h>
 
 int dxgk_create_sync_file(struct dxgprocess *process, void *__user inargs);
+
+/*
+ * The object is used to pass additional data required for a callback
+ * for a sync file.
+ */
+struct dxg_sync_cb_t {
+	/* callback for dma_fence_add_callback() */
+	struct dma_fence_cb  cb;
+	struct d3dkmthandle  device;
+	struct dxgprocess  *process;
+	struct dxgadapter  *adapter;
+	__u32			object_count;
+#ifdef __KERNEL__
+	struct d3dkmthandle  *objects;
+	__u64	             *fence_values;
+#else
+	__u64                objects;
+	__u64                fence_values;
+#endif
+    /* node in the list of callbacks.*/
+	struct list_head    node;
+};
 
 struct dxgsyncpoint {
 	struct dxghostevent	hdr;
 	struct dma_fence	base;
+    /* List of dxg_sync_cb_t */
+	struct list_head sync_cb_list;
+	/* lock around sync_cb_list */
+	struct mutex sync_cb_mutex;
 	u64			fence_value;
 	u64			context;
+	/* The irqsafe spinlock to use for locking base. */
 	spinlock_t		lock;
 	u64			u64;
 };
