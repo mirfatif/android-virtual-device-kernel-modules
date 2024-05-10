21bc0b1c4c4be08740b7fc04efd77aa7a97aaabe
Shalini S <shalinisen@google.com>
2024-05-10 18:28:48 +0000

    SignalSynchronizationObjectFromSyncFile api
    - Add context in dxgsyncpoint for handles/fences
    - Add api SignalSynchronizationObjectFromSyncFile. Registers callback on
      dma fence.
    - Implement callback
    
    Change-Id: I952ca5c105c23289cb48b64a90235e009c6d428e

--- virtio_dxgkrnl/dxgsyncfile.h
+++ virtio_dxgkrnl/dxgsyncfile.h
@@ -18,9 +18,30 @@
 
 int dxgk_create_sync_file(struct dxgprocess *process, void *__user inargs);
 
+/*
+ * The object is used to pass additional data required for a callback
+ * for a sync file.
+ */
+struct dxg_sync_cb_t {
+	/* callback for dma_fence_add_callback() */
+	struct dma_fence_cb cb;
+	struct d3dkmthandle device;
+#ifdef __KERNEL__
+	struct d3dkmthandle	*objects;
+	__u64			    *fence_values;
+#else
+	__u64			    objects;
+	__u64			    fence_values;
+#endif
+    /* list_head to be used to add dxg_sync_cb_t to a list of callbacks.*/
+	struct list_head    list;
+};
+
 struct dxgsyncpoint {
 	struct dxghostevent	hdr;
 	struct dma_fence	base;
+    /* List of dxg_sync_cb_t */
+	struct list_head sync_cb_list;
 	u64			fence_value;
 	u64			context;
 	spinlock_t		lock;
