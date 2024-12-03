8b6169bd05a0853078ae9884aa6ad7d5453704f8
Kaiyi Li <kaiyili@google.com>
2024-12-02 17:30:11 -0800

    ANDROID: virtio-dxgkrnl: when mapping memory grab the write lock and set VM_DONTCOPY
    We refcount the mapped memory in lock2/unlock2. However, that doesn't
    work well with fork - when unlock2 in one process destroys that memory
    mapping, there is another page table entry left in the other process
    still pointing to that same GPA. And this can confuse the PAT tracing,
    and cause memory mapping on the same GPA to fail under certain
    circumstances.
    
    Test: presubmit
    Bug: b/364907812
    Change-Id: Idf74ed2d9c41683bb6fb821d26f698ab79a7f8bf
    Signed-off-by: Kaiyi Li <kaiyili@google.com>

--- virtio_dxgkrnl/dxgvmbus.c
+++ virtio_dxgkrnl/dxgvmbus.c
@@ -253,7 +253,12 @@ static u8 *dxg_map_iospace(u64 iospace_address, u32 size,
 		return NULL;
 	}
 
-	mmap_read_lock(current->mm);
+	// TODO: propagate the error to the caller or retry right away.
+	if (mmap_write_lock_killable(current->mm)) {
+		pr_err("%s: mmap_write_lock_killable failed", __func__);
+		dxg_unmap_iospace((void *)va, size);
+		return NULL;
+	}
 	vma = find_vma(current->mm, (unsigned long)va);
 	if (vma) {
 		pgprot_t prot = vma->vm_page_prot;
@@ -263,6 +268,9 @@ static u8 *dxg_map_iospace(u64 iospace_address, u32 size,
 		dev_dbg(dxgglobaldev, "vma: %lx %lx %lx",
 			    vma->vm_start, vma->vm_end, va);
 		vma->vm_pgoff = iospace_address >> PAGE_SHIFT;
+		// We don't allow child processes to inherit the pages, because the
+		// mapping is not correctly refcounted after fork'ed.
+		vma->vm_flags |= VM_DONTCOPY;
 		ret = io_remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
 					 size, prot);
 		if (ret)
